b2f8f38ed8a7c7479492295f22c75a273856d7a7
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2021-09-21 11:37:37 -0300

    Commit com a criação do projeto

---
+++ src/main/java/br/com/finchsolucoes/xgracco/XgraccoApplication.java
@@ -0,0 +1,13 @@
+package br.com.finchsolucoes.xgracco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgraccoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XgraccoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/finchsolucoes/xgracco/XgraccoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finchsolucoes.xgracco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XgraccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
