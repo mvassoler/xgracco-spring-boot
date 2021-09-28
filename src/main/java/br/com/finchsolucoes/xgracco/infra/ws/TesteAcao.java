9c45c31ef115c473b41b84d72c9fd946d1c1e5fa
Gabriel <gabrielr01oficial@gmail.com>
2021-09-28 14:19:24 -0300

    Adição do teste para Ação

--- src/main/java/br/com/finchsolucoes/xgracco/infra/ws/TesteAcao.java
+++ src/main/java/br/com/finchsolucoes/xgracco/infra/ws/TesteAcao.java
@@ -1,10 +1,14 @@
 package br.com.finchsolucoes.xgracco.infra.ws;
 
 import br.com.finchsolucoes.xgracco.domain.entity.Acao;
+import br.com.finchsolucoes.xgracco.domain.query.Query;
+import br.com.finchsolucoes.xgracco.domain.query.Sorter;
+import br.com.finchsolucoes.xgracco.domain.query.impl.AcaoFilter;
 import br.com.finchsolucoes.xgracco.domain.repository.AcaoRepository;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -18,7 +22,18 @@ public class TesteAcao {
 
     @PostConstruct
     public void testar(){
+
+        acaoRepository.save(Acao.builder().descricao("test").build());
+        Query<Acao> query = Query.<Acao>builder()
+                .filter(new AcaoFilter(null, null, null))
+                .sort(Sorter.<Acao>by("descricao").direction(Sorter.Direction.ASC))
+                .page(1l)
+                .build();
+
         Optional<Acao> acao = acaoRepository.findById(1l);
+        List<Acao> listAcao = acaoRepository.find(query);
+
+        System.out.println(listAcao);
         System.out.println("Teste");
 
 
