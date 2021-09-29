bbbaa5da47ad55e2b5733d2cafadfadc7585630f
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2021-09-29 13:57:02 -0300

    Commit com inclusão de variáveis para o database no properties e retorna da classe TarefaStatusFinalAgendamentoRepositoryImpl.

--- src/main/java/br/com/finchsolucoes/xgracco/domain/repository/TarefaStatusFinalAgendamentoRepository.java
+++ src/main/java/br/com/finchsolucoes/xgracco/domain/repository/TarefaStatusFinalAgendamentoRepository.java
@@ -1,9 +1,9 @@
-//package br.com.finchsolucoes.xgracco.domain.repository;
-//
-//import br.com.finchsolucoes.xgracco.domain.entity.TarefaStatusFinalAgendamento;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface TarefaStatusFinalAgendamentoRepository extends JpaRepository<TarefaStatusFinalAgendamento,Long>, TarefaStatusFinalJpaRepository {
-//}
+package br.com.finchsolucoes.xgracco.domain.repository;
+
+import br.com.finchsolucoes.xgracco.domain.entity.TarefaStatusFinalAgendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaStatusFinalAgendamentoRepository extends JpaRepository<TarefaStatusFinalAgendamento,Long>, TarefaStatusFinalJpaRepository {
+}
