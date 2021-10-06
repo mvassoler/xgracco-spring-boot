9cd4922a76be9fa3a8551c63c29837fe9954f622
Gabriel <gabrielr01oficial@gmail.com>
2021-10-06 08:59:47 -0300

    ajustes

--- src/main/java/br/com/finchsolucoes/xgracco/XgraccoApplication.java
+++ src/main/java/br/com/finchsolucoes/xgracco/XgraccoApplication.java
@@ -2,10 +2,6 @@ package br.com.finchsolucoes.xgracco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication(scanBasePackages = {"br.com.finchsolucoes"})
 public class XgraccoApplication {
--- src/main/java/br/com/finchsolucoes/xgracco/domain/dto/input/VaraDTO.java
+++ src/main/java/br/com/finchsolucoes/xgracco/domain/dto/input/VaraDTO.java
@@ -1,5 +1,7 @@
 package br.com.finchsolucoes.xgracco.domain.dto.input;
 
+import br.com.finchsolucoes.xgracco.domain.enums.EnumInstancia;
+import br.com.finchsolucoes.xgracco.domain.enums.EnumTipoJustica;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @Builder
@@ -19,6 +22,10 @@ public class VaraDTO {
     @NotBlank(message = "{entity.description.required}")
     @Size(min = 1, max = 100,  message = "{entity.description.max.lenght}")
     private String descricao;
+
+    private List<EnumTipoJustica> tiposJustica;
+
+    private List<EnumInstancia> instancias;
 
 
 }
